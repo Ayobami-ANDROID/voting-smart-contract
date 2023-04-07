61a23ddb63e7e3c196aaaa3a248713f913e7a9aa
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2023-04-08 00:42:02 +0100

    voting

--- contracts/voting.sol
+++ contracts/voting.sol
@@ -16,8 +16,21 @@ contract voting{
         uint end_time;
         uint fee;
         uint option_count; // Number of options
-        mapping(uint => VoteOption ) options; // mapping of index+1 to options (should start from 1)
+        mapping(uint => voteOption ) options; // mapping of index+1 to options (should start from 1)
         string[] option_names; // array of option names;
         mapping(address => uint ) votes; // mapping of address to option_id (should start from 1)
+    }
+
+    struct GetVotesReturn {
+       address[] poll_owners;
+       string[] poll_names;
+       string[] poll_descriptions;
+       uint[] poll_start_times;
+       uint[] poll_end_times;
+       uint[] poll_fees;
+       uint[] poll_ids;
+       string[][] poll_options;
+       uint[][] poll_options_votes;
+       uint[] poll_voted_idxs;
     }
 }
