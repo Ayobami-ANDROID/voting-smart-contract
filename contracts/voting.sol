ce7555fee83592bf1af6662f19f75f4ac926a1ec
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2023-04-10 21:39:12 +0100

    voting

--- contracts/voting.sol
+++ contracts/voting.sol
@@ -100,4 +100,64 @@ contract voting{
         address_to_username[msg.sender] = username;
         username_to_address[username] = msg.sender;
     }
+
+    function getPolls() public view returns (GetVotesReturn memory poll_data) {
+       address[] memory owners = new address[](polls_created);
+       string[] memory names = new string[](polls_created);
+       string[] memory descriptions = new string[](polls_created);
+       uint[] memory start_times = new uint[](polls_created);
+       uint[] memory end_times = new uint[](polls_created);
+       uint[] memory fees = new uint[](polls_created);
+       uint[] memory poll_ids = new uint[](polls_created);
+       uint[] memory voted_idxs = new uint[](polls_created);
+
+        // array of arrays for options
+       string[][] memory option_list = new string[][](polls_created);
+        // array of arrays for options votes
+        uint[][] memory option_vote_list = new uint[][](polls_created);
+        for (uint i=0; i<polls_created; i++) {
+            owners[i] = polls[i].owner;
+            names[i] = polls[i].name;
+            descriptions[i] = polls[i].description;
+            start_times[i] = polls[i].start_time;
+            end_times[i] = polls[i].end_time;
+            fees[i] = polls[i].fee;
+            poll_ids[i] = i;
+            voted_idxs[i] = polls[i].votes[msg.sender];
+            
+            // get option_count from poll
+            uint option_count = polls[i].option_count;
+            uint[] memory option_votes = new uint[](option_count);
+            for (uint j=0; j<option_count; j++) {
+                voteOption memory option = polls[i].options[j+1];
+                option_votes[j] = option.count;
+            }
+
+            // add options to option_list
+            option_list[i] = polls[i].option_names;
+            // add options_votes to option_vote_list
+            option_vote_list[i] = option_votes;
+            
+        }
+        return GetVotesReturn(owners, names, descriptions, start_times, end_times, fees, poll_ids, option_list, option_vote_list, voted_idxs);
+    }
+     function vote(uint poll_id, uint option_id) payable public {
+        // Check that poll exists
+        require(poll_id < polls_created, "Poll does not exist");
+        // Check that option exists
+        require(option_id < polls[poll_id].option_count, "Option does not exist");
+        // Check that user has not already voted
+        require(polls[poll_id].votes[msg.sender] == 0, "User has already voted");
+        // Check that user has not voted in the future
+        require(polls[poll_id].start_time <= block.timestamp, "User has tried voting in the future");
+        // Check that user has not voted in the past
+        require(polls[poll_id].end_time >= block.timestamp, "User has tried voting for the past");
+        // Check that user has enough balance
+        require(msg.value >= polls[poll_id].fee, "User does not have enough balance");
+        // set poll.votes of user to option_id
+        polls[poll_id].votes[msg.sender] = option_id+1;
+        polls[poll_id].options[option_id+1].count++;
+        // transfer the balance to the poll owner
+        payable(polls[poll_id].owner).transfer(polls[poll_id].fee);
+    }
 }
